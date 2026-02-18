3f273a4c455f36eec3773a2b917db32dc2eef98b
bartolomeocalmo <bartolomeocalmo0@gmail.com>
2026-02-18 21:22:03 +0100

    completamento progetto automatico + pagine legali

--- app.py
+++ app.py
@@ -575,3 +575,5 @@ if __name__ == "__main__":
     print("  ✅ APRI IL BROWSER SU: http://127.0.0.1:5000/")
     print("=" * 70)
     print("\n")
+
+    app.run(host="127.0.0.1", port=5000, debug=True)
